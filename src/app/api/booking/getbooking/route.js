f0c646a4364c20a35815ad2c2d0a154749c3079d
navraj213 <nsingh59@nyit.edu>
2024-12-03 02:10:27 -0500

    Add getboking for student side
    Co-authored-by: Anmolak21 <Anmolak21@users.noreply.github.com>

--- src/app/api/booking/getbooking/route.js
+++ src/app/api/booking/getbooking/route.js
@@ -3,83 +3,16 @@ import mysql from 'mysql2/promise';
 export async function POST(req, res) {
     try {
         // Parse request body
-        const { reserverId, reserverType } = await req.json();
+        const { reserverId, reserverType, date, roomId } = await req.json();
 
-        if (!reserverId || !reserverType || !roomId || !date || !startTime || !endTime) {
-            return new Response(JSON.stringify({ message: 'Invalid Data' }), {
-                status: 400,
-            });
-        }
-
-        // Create a new connection for the request
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'Database28',
-            database: 'Roomy',
-        });
-
-        console.log('Connected to MySQL');
-
-        // Check if the student has an approved booking for the same day
-        const [existingBookings] = await connection.execute(
-            'SELECT * FROM Bookings WHERE reserver_id = ? AND reserver_type = ? AND date = ? AND status = "Approved"',
-            [reserverId, reserverType, date]
-        );
-
-        // Check if there's an existing booking for the same room and time
-        const [conflictingBookings] = await connection.execute(
-            'SELECT * FROM Bookings WHERE room_id = ? AND date = ? AND ((start_time < ? AND end_time > ?) OR (start_time < ? AND end_time > ?))',
-            [roomId, date, endTime, startTime, startTime, endTime]
-        );
-
-        let status = 'Pending';
-
-        // Logic for status determination
-        if (existingBookings.length === 0) {
-            // If no approved booking exists for the day, approve this one
-            status = 'Approved';
-        }
-
-        if (conflictingBookings.length > 0) {
-            const [conflict] = conflictingBookings;
-            if (conflict.status === 'Pending') {
-                // Decline the conflicting pending booking
-                await connection.execute(
-                    'UPDATE Bookings SET status = "Declined" WHERE booking_id = ?',
-                    [conflict.booking_id]
-                );
-
-                // Approve this booking
-                status = 'Approved';
+        if (!reserverId && !reserverType) {
+            if (!date) {
+                return new Response(JSON.stringify({ message: 'Invalid request' }), {
+                    status: 400,
+                });
             }
         }
 
-        // Insert the new booking
-        const [result] = await connection.execute(
-            'INSERT INTO Bookings (reserver_id, reserver_type, room_id, date, start_time, end_time, status) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [reserverId, reserverType, roomId, date, startTime, endTime, status]
-        );
-
-        // Close the connection
-        await connection.end();
-
-        console.log('Booking created:', result);
-
-        return new Response(
-            JSON.stringify({ message: 'Booking created', bookingId: result.insertId, status }),
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error('Error occurred:', error);
-        return new Response(JSON.stringify({ message: 'Try again later.' }), {
-            status: 500,
-        });
-    }
-}
-
-export async function GET(req, res) {
-    try {
         // Create a new connection for the request
         const connection = await mysql.createConnection({
             host: 'localhost',
@@ -88,15 +21,25 @@ export async function GET(req, res) {
             database: 'Roomy',
         });
 
-        console.log('Connected to MySQL');
-
-        // Fetch all bookings
-        const [bookings] = await connection.execute('SELECT * FROM Bookings');
+        let query = 'SELECT * FROM Bookings WHERE ';
+        let result;
+        if (reserverId && reserverType) {
+            query += 'reserver_id = ? AND reserver_type = ?';
+            [result] = await connection.execute(query, [reserverId, reserverType]);
+        } else if (date) {
+            query += 'date = ?';
+            const formattedDate = new Date(new Date(date).setDate(new Date(date).getDate() + 1)).toISOString().split('T')[0];
+            [result] = await connection.execute(query, [formattedDate]);
+        }
 
         // Close the connection
         await connection.end();
 
-        return new Response(JSON.stringify(bookings), { status: 200 });
+
+        return new Response(
+            JSON.stringify({ message: 'Booking Retrieved', bookings: result }),
+            { status: 200 }
+        );
     } catch (error) {
         console.error('Error occurred:', error);
         return new Response(JSON.stringify({ message: 'Try again later.' }), {
--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { use, useState, useEffect } from 'react';
 import Image from "next/image";
 
 const StudentDashboard: React.FC = () => {
@@ -10,6 +10,21 @@ const StudentDashboard: React.FC = () => {
     const [isBookingOpen, setIsBookingOpen] = useState(false);
     const [selectedRoom, setSelectedRoom] = useState<number | null>(null);
     const [selectedDate, setSelectedDate] = useState<string>(new Date().toLocaleDateString('en-CA').split('T')[0]);
+    const [allBookings, setAllBookings] = useState<any[]>([]);
+
+    type Booking = {
+        booking_id: number;
+        reserver_id: number;
+        reserver_type: string;
+        room_id: number;
+        date: string;
+        start_time: string;
+        end_time: string;
+        status: string;
+    };
+
+    const [myBookings, setMyBookings] = useState<Booking[]>([]);
+
     type StudyRoom = {
         room_id: number;
         location: string;
@@ -65,7 +80,6 @@ const StudentDashboard: React.FC = () => {
                 };
 
                 localStorage.setItem('student', JSON.stringify(student as Student));
-                console.log("Student data refreshed");
                 return;
             } else {
                 return;
@@ -124,8 +138,6 @@ const StudentDashboard: React.FC = () => {
             const startTime24 = convertTo24HourFormat(selectedTime);  // Convert start time
             const startHour = parseInt(startTime24.split(':')[0], 10);
             const endTime24 = `${(startHour + 1).toString().padStart(2, '0')}:00:00`;  // End time + 1 hour
-
-            console.log(new Date(selectedDate).toISOString().split('T')[0]);
     
             // Send request to backend with valid formatted date and time
             const res = await fetch('/api/booking/addbooking', {
@@ -151,13 +163,54 @@ const StudentDashboard: React.FC = () => {
             alert("Booking failed.");
         }
     };
-    
 
-    const timeSlots = ["12pm-1pm", "1pm-2pm", "2pm-3pm", "3pm-4pm", "4pm-5pm"];
-    const bookings = [
-        { id: 1, room: "Anna Rubin", time: "12pm-1pm", status: "Confirmed" },
-        { id: 2, room: "Sultran Center", time: "2pm-3pm", status: "Pending" },
-    ];
+    const getBookingsForStudent = async () => {
+        try {
+            const res = await fetch('/api/booking/getbooking', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ reserverId: currStudent?.student_id, reserverType: 'Student' }),
+            });
+
+            if (res.ok) {
+                const {bookings} = await res.json();
+                setMyBookings(bookings);
+                return;
+            } else {
+                return;
+            }
+        } catch (error) {
+            return;
+        }
+    }
+
+    if (!myBookings || myBookings.length === 0) {
+        getBookingsForStudent();
+    }
+
+    const getBookingsByDate = async () => {
+        try {
+            const res = await fetch('/api/booking/getbooking', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ date: new Date(selectedDate).toLocaleDateString().split('T')[0] }),
+            });
+
+            if (res.ok) {
+                const {bookings} = await res.json();
+                setAllBookings(bookings);
+                return;
+            } else {
+                return;
+            }
+        } catch (error) {
+            return;
+        }
+    }
+
+    useEffect(() => {
+        getBookingsByDate();
+    }, [selectedDate]);
 
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-r from-yellow-100 to-orange-200 text-black">
@@ -178,38 +231,34 @@ const StudentDashboard: React.FC = () => {
             <div className="flex flex-col items-center px-5">
                 <h1 className="text-4xl font-extrabold py-5">Welcome {currStudent?.full_name}!</h1>
 
-                {/* Room Booking Panel */}
-                <button
-                    className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-400"
-                    onClick={() => setIsBookingOpen(true)}
-                >
-                    Reserve a Room
-                </button>
-
                 {/* My Bookings Section */}
                 <div className="w-full max-w-4xl mt-10 bg-white p-5 rounded-lg shadow-lg">
                     <h2 className="text-2xl font-bold mb-4">My Bookings</h2>
                     <ul>
-                        {bookings.map((booking) => (
-                            <li
-                                key={booking.id}
-                                className="flex justify-between items-center p-3 border-b border-gray-300"
-                            >
-                                <div>
-                                    <p className="font-bold">{booking.room}</p>
-                                    <p>{booking.time}</p>
-                                </div>
-                                <span
-                                    className={`px-3 py-1 rounded-full text-white ${
-                                        booking.status === "Confirmed"
-                                            ? "bg-green-500"
-                                            : "bg-yellow-500"
-                                    }`}
+                        {myBookings && myBookings
+                            .filter(booking => new Date(booking.date) >= new Date(new Date().toLocaleDateString('en-CA')))
+                            .map((booking) => (
+                                <li
+                                    key={booking.booking_id}
+                                    className="flex justify-between items-center p-3 border-b border-gray-300"
                                 >
-                                    {booking.status}
-                                </span>
-                            </li>
-                        ))}
+                                    <div>
+                                        <p className="font-bold">{booking.room_id}</p>
+                                        <p>{(booking.date).split('T')[0]} {booking.start_time} - {booking.end_time}</p>
+                                    </div>
+                                    <span
+                                        className={`px-3 py-1 rounded-full text-white ${
+                                            booking.status === "Approved"
+                                                ? "bg-green-500"
+                                                : booking.status === "Declined"
+                                                ? "bg-red-500"
+                                                : "bg-yellow-500"
+                                        }`}
+                                    >
+                                        {booking.status}
+                                    </span>
+                                </li>
+                            ))}
                     </ul>
                 </div>
 
@@ -244,7 +293,9 @@ const StudentDashboard: React.FC = () => {
                             type="date"
                             className="w-full p-2 border border-gray-300 rounded-lg mb-4"
                             value={selectedDate || new Date().toLocaleDateString('en-CA')}
-                            onChange={(e) => setSelectedDate(e.target.value)}
+                            onChange={(e) => {
+                                setSelectedDate(e.target.value);
+                            }}
                             min={new Date().toLocaleDateString('en-CA')}
                         />
                         <div className="mb-6">
@@ -269,26 +320,65 @@ const StudentDashboard: React.FC = () => {
                                                         <div className="w-full max-w-4xl mt-10 p-5">
                                                             <div className="flex overflow-x-auto space-x-2">
                                                                 {Array.from({ length: 12 }, (_, i) => {
-                                                                    const hour = 8 + i;
-                                                                    const time = `${hour % 12 || 12}${hour < 12 ? 'am' : 'pm'}`;
-                                                                    return (
-                                                                        <div
-                                                                            key={i}
-                                                                            className={`p-1 rounded-lg text-center text-sm min-w-[50px] cursor-pointer ${
-                                                                                selectedTime === time && selectedBuilding === room.location && selectedRoom === room.room_id ? 'bg-yellow-500' : 'bg-green-500 text-white hover:bg-gray-300'
-                                                                            }`}
-                                                                            onClick={() => {
-                                                                                setSelectedTime(time);
-                                                                                setSelectedBuilding(room.location);
-                                                                                setSelectedRoom(room.room_id);
-                                                                            }}
-                                                                        >
-                                                                            {time}
-                                                                        </div>
-                                                                    );
+                                                                const hour = 8 + i;
+                                                                const time = `${hour % 12 || 12}${hour < 12 ? 'am' : 'pm'}`;
+
+                                                                const convertTo24HourFormat = (time : string) => {
+                                                                    const [hourMin, period] = time.split(/(am|pm)/);
+                                                                    let [hours, minutes] = hourMin.split(':').map((part) => parseInt(part, 10)); // Parse as integers
+                                                                
+                                                                    // Ensure that hours and minutes are valid numbers
+                                                                    if (isNaN(hours)) hours = 0;
+                                                                    if (isNaN(minutes)) minutes = 0;
+                                                                
+                                                                    // Convert to 24-hour format
+                                                                    if (period === 'pm' && hours !== 12) {
+                                                                        hours += 12; // Convert PM to 24-hour format
+                                                                    }
+                                                                    if (period === 'am' && hours === 12) {
+                                                                        hours = 0; // Convert midnight (12 AM) to 00:00
+                                                                    }
+                                                                
+                                                                    // Subtract 1 hour
+                                                                    hours = (hours + 1 + 24) % 24;
+                                                                
+                                                                    // Return time as HH:MM:00 string (with leading zeros if necessary)
+                                                                    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00`;
+                                                                };
+
+                                                                // Check if the current time slot is already taken
+                                                                const isTaken = allBookings && allBookings.some(
+                                                                    (booking) =>
+                                                                    booking.status === 'Approved' &&
+                                                                    booking.room_id === room.room_id &&
+                                                                    booking.date.split('T')[0] === (selectedDate).split('T')[0] &&
+                                                                    booking.start_time === convertTo24HourFormat(time)
+                                                                );
+
+                                                                return (
+                                                                    <div
+                                                                        key={i}
+                                                                        className={`p-1 rounded-lg text-center text-sm min-w-[50px] cursor-pointer ${
+                                                                            isTaken
+                                                                            ? 'bg-red-500 text-white cursor-not-allowed' // Red if taken and not clickable
+                                                                            : selectedRoom === room.room_id && selectedTime === time
+                                                                            ? 'bg-yellow-500' // Highlight if selected
+                                                                            : 'bg-green-500 text-white hover:bg-gray-300'
+                                                                        }`}
+                                                                        onClick={() => {
+                                                                            if (!isTaken) {
+                                                                            setSelectedTime(time);
+                                                                            setSelectedBuilding(room.location);
+                                                                            setSelectedRoom(room.room_id);
+                                                                            }
+                                                                        }}
+                                                                    >
+                                                                        {time}
+                                                                    </div>
+                                                                );
                                                                 })}
                                                             </div>
-                                                        </div>
+                                                            </div>
                                                     </li>
                                                 ))}
                                         </div>
@@ -345,21 +435,6 @@ const StudentDashboard: React.FC = () => {
                             {Array.from(new Set(studyRooms.map(room => room.location))).sort().map((building: string) => (
                                 <option key={building} value={building}>
                                     {building}
-                                </option>
-                            ))}
-                        </select>
-
-                        {/* Time Slot Selection */}
-                        <label className="block text-lg font-bold mb-2">Choose a Time Slot:</label>
-                        <select
-                            className="w-full p-2 border border-gray-300 rounded-lg mb-4"
-                            value={selectedTime}
-                            onChange={(e) => setSelectedTime(e.target.value)}
-                        >
-                            <option value="">Select a time slot</option>
-                            {timeSlots.map((slot) => (
-                                <option key={slot} value={slot}>
-                                    {slot}
                                 </option>
                             ))}
                         </select>
